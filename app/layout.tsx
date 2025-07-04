6c61cdddb66838c99ade4fc9fda394b3bf99188d
yeasinhasan22 <amyasxtrem@gmail.com>
2025-07-04 22:40:18 +0600

    another

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import './globals.css';
 import type { Metadata } from 'next';
 import { Josefin_Sans } from 'next/font/google';
@@ -21,6 +22,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <Head>
+        <title>Yeasin Hasan | Full-Stack Developer Portfolio</title>
+        <meta name="description" content="I'm a full-stack developer building modern, fast, and responsive web applications with MERN & Next.js." />
+
+        <meta property="og:title" content="Yeasin Hasan | Software Engineer Portfolio" />
+        <meta property="og:description" content="Explore my personal portfolio — a showcase of modern web development projects, skills, and experiences." />
+        <meta property="og:image" content="/images/preview.webp" />
+        <meta property="og:url" content="https://yeasinhasan.vercel.app/" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Yeasin Hasan | Software Engineer Portfolio" />
+        <meta name="twitter:description" content="Explore my personal portfolio — a showcase of modern web development projects, skills, and experiences." />
+        <meta name="twitter:image" content="/images/preview.webp" />
+      </Head>
       <body className={josefinSans.className}>
         <CustomCursor/>
         <ThemeProvider
