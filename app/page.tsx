9618a4349eb8bea0f75cbf9fb376b481651cb09f
yeasinhasan22 <amyasxtrem@gmail.com>
2025-07-04 22:24:48 +0600

    preview update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Navbar from "@/components/navbar";
 import HeroSection from "@/components/hero-section";
 import ExperienceSection from "@/components/experience-section";
@@ -8,14 +9,32 @@ import SimpleNavbar from "@/components/SimpleNavbar";
 
 export default function Home() {
   return (
-    <main className="min-h-screen relative">
-      <Navbar />
-      <SimpleNavbar />
-      <HeroSection />
-      <ExperienceSection />
-      <SkillsSection />
-      <ProjectsSection />
-      <ContactSection />
-    </main>
+    <>
+      <Head>
+        <title>Yeasin Hasan | Full-Stack Developer Portfolio</title>
+        <meta name="description" content="I'm a full-stack developer building modern, fast, and responsive web applications with MERN & Next.js." />
+
+        <meta property="og:title" content="Yeasin Hasan | Software Engineer Portfolio" />
+        <meta property="og:description" content="Explore my personal portfolio — a showcase of modern web development projects, skills, and experiences." />
+        <meta property="og:image" content="/images/preview.webp" />
+        <meta property="og:url" content="https://yeasinhasan.vercel.app/" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Yeasin Hasan | Software Engineer Portfolio" />
+        <meta name="twitter:description" content="Explore my personal portfolio — a showcase of modern web development projects, skills, and experiences." />
+        <meta name="twitter:image" content="/images/preview.webp" />
+      </Head>
+
+      <main className="min-h-screen relative">
+        <Navbar />
+        <SimpleNavbar />
+        <HeroSection />
+        <ExperienceSection />
+        <SkillsSection />
+        <ProjectsSection />
+        <ContactSection />
+      </main>
+    </>
   );
 }
